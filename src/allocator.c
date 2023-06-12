0b0e334fa9ea55113850957021652b1611411f9d
Patrick Hao <patrick.hao95@gmail.com>
2023-06-12 22:53:14 +1000

    Reindented allocator.c.

--- src/allocator.c
+++ src/allocator.c
@@ -29,110 +29,100 @@
 #endif
 
 static void *
-__default_allocate(size_t size, void * state)
-{
-  RCUTILS_CAN_RETURN_WITH_ERROR_OF(NULL);
+__default_allocate(size_t size, void *state) {
+    RCUTILS_CAN_RETURN_WITH_ERROR_OF(NULL);
 
-  RCUTILS_UNUSED(state);
-  return malloc(size);
+    RCUTILS_UNUSED(state);
+    return malloc(size);
 }
 
 static void
-__default_deallocate(void * pointer, void * state)
-{
-  RCUTILS_UNUSED(state);
-  free(pointer);
+__default_deallocate(void *pointer, void *state) {
+    RCUTILS_UNUSED(state);
+    free(pointer);
 }
 
 static void *
-__default_reallocate(void * pointer, size_t size, void * state)
-{
-  RCUTILS_CAN_RETURN_WITH_ERROR_OF(NULL);
+__default_reallocate(void *pointer, size_t size, void *state) {
+    RCUTILS_CAN_RETURN_WITH_ERROR_OF(NULL);
 
-  RCUTILS_UNUSED(state);
-  return realloc(pointer, size);
+    RCUTILS_UNUSED(state);
+    return realloc(pointer, size);
 }
 
 static void *
-__default_zero_allocate(size_t number_of_elements, size_t size_of_element, void * state)
-{
-  RCUTILS_CAN_RETURN_WITH_ERROR_OF(NULL);
+__default_zero_allocate(size_t number_of_elements, size_t size_of_element, void *state) {
+    RCUTILS_CAN_RETURN_WITH_ERROR_OF(NULL);
 
-  RCUTILS_UNUSED(state);
-  return calloc(number_of_elements, size_of_element);
+    RCUTILS_UNUSED(state);
+    return calloc(number_of_elements, size_of_element);
 }
 
 rcutils_allocator_t
-rcutils_get_zero_initialized_allocator(void)
-{
-  static rcutils_allocator_t zero_allocator = {
-    .allocate = NULL,
-    .deallocate = NULL,
-    .reallocate = NULL,
-    .zero_allocate = NULL,
-    .state = NULL,
-  };
-  return zero_allocator;
+rcutils_get_zero_initialized_allocator(void) {
+    static rcutils_allocator_t zero_allocator = {
+            .allocate = NULL,
+            .deallocate = NULL,
+            .reallocate = NULL,
+            .zero_allocate = NULL,
+            .state = NULL,
+    };
+    return zero_allocator;
 }
 
 static rcutils_allocator_t default_allocator = {
-    .allocate = __default_allocate,
-    .deallocate = __default_deallocate,
-    .reallocate = __default_reallocate,
-    .zero_allocate = __default_zero_allocate,
-    .state = NULL,
-  };
+        .allocate = __default_allocate,
+        .deallocate = __default_deallocate,
+        .reallocate = __default_reallocate,
+        .zero_allocate = __default_zero_allocate,
+        .state = NULL,
+};
 
 bool
-rcutils_set_default_allocator(rcutils_allocator_t * allocator){
-  if (rcutils_allocator_is_valid(allocator))
-  {
-    default_allocator.allocate = allocator->allocate;
-    default_allocator.deallocate = allocator->deallocate;
-    default_allocator.reallocate = allocator->reallocate;
-    default_allocator.zero_allocate = allocator->zero_allocate;
-    default_allocator.state = NULL;
-    return true;
-  }
-  return false;
+rcutils_set_default_allocator(rcutils_allocator_t *allocator) {
+    if (rcutils_allocator_is_valid(allocator)) {
+        default_allocator.allocate = allocator->allocate;
+        default_allocator.deallocate = allocator->deallocate;
+        default_allocator.reallocate = allocator->reallocate;
+        default_allocator.zero_allocate = allocator->zero_allocate;
+        default_allocator.state = NULL;
+        return true;
+    }
+    return false;
 }
 
 rcutils_allocator_t
-rcutils_get_default_allocator()
-{
-  return default_allocator;
+rcutils_get_default_allocator() {
+    return default_allocator;
 }
 
 bool
-rcutils_allocator_is_valid(const rcutils_allocator_t * allocator)
-{
-  if (
-    NULL == allocator ||
-    NULL == allocator->allocate ||
-    NULL == allocator->deallocate ||
-    NULL == allocator->zero_allocate ||
-    NULL == allocator->reallocate)
-  {
-    return false;
-  }
-  return true;
+rcutils_allocator_is_valid(const rcutils_allocator_t *allocator) {
+    if (
+            NULL == allocator ||
+            NULL == allocator->allocate ||
+            NULL == allocator->deallocate ||
+            NULL == allocator->zero_allocate ||
+            NULL == allocator->reallocate) {
+        return false;
+    }
+    return true;
 }
 
 void *
-rcutils_reallocf(void * pointer, size_t size, rcutils_allocator_t * allocator)
-{
-  if (!rcutils_allocator_is_valid(allocator)) {
-    // cannot deallocate pointer, so print message to stderr and return NULL
+rcutils_reallocf(void *pointer, size_t size, rcutils_allocator_t *allocator) {
+    if (!rcutils_allocator_is_valid(allocator)) {
+        // cannot deallocate pointer, so print message to stderr and return NULL
 #if RCUTILS_REPORT_ERROR_HANDLING_ERRORS
-    RCUTILS_SAFE_FWRITE_TO_STDERR(
-      "[rcutils|allocator.c:" RCUTILS_STRINGIFY(__LINE__) "] rcutils_reallocf(): "
-      "invalid allocator or allocator function pointers, memory leaked\n");
+        RCUTILS_SAFE_FWRITE_TO_STDERR(
+                "[rcutils|allocator.c:" RCUTILS_STRINGIFY(__LINE__) "] rcutils_reallocf(): "
+                                                                    "invalid allocator or allocator function pointers, memory leaked\n");
 #endif
-    return NULL;
-  }
-  void * new_pointer = allocator->reallocate(pointer, size, allocator->state);
-  if (NULL == new_pointer) {
-    allocator->deallocate(pointer, allocator->state);
-  }
-  return new_pointer;
+        return NULL;
+    }
+    void *new_pointer = allocator->reallocate(pointer, size, allocator->state);
+    if (NULL == new_pointer) {
+        allocator->deallocate(pointer, allocator->state);
+    }
+    return new_pointer;
 }
